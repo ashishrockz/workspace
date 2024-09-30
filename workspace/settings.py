0f5c0195613a22efea5a95421c242fc3e92174ce
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-09-30 22:54:44 +0530

    project setup 2.0

--- workspace/settings.py
+++ workspace/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_management',
-    'organistion',
     'home'
 ]
 
@@ -119,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS=[
+    BASE_DIR /"static"
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+MIDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
